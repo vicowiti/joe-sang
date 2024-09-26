c461dfd857d481fa19d7d6a96a745ef3da4ccb75
Victor <victor.owiti@zeraki.app>
2024-09-26 15:26:12 +0300

    Loading

--- app/components/Donate/DonationInput.tsx
+++ app/components/Donate/DonationInput.tsx
@@ -8,10 +8,11 @@ import axios from "axios";
 const DonationInput = () => {
   const [amount, setAmount] = useState("");
   const [phone, setPhone] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleDonate = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+setLoading(true)
     const response = await axios.post(
       "https://joe-sang.netlify.app/api/donations",
       {
@@ -20,8 +21,10 @@ const DonationInput = () => {
       }
     );
 
+    setLoading(false)
+
     if (response.status === 200) {
-      alert("Wait fo mpesa popup");
+      alert("Wait for mpesa popup");
     } else {
       alert("Donation failed");
     }
@@ -38,6 +41,9 @@ const DonationInput = () => {
       </div>
 
       <button className="text-white bg-[#9B9BFF] flex items-center justify-center gap-2 p-3 font-bold my-2 rounded-md">
+        {loading && (
+          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-200"></div>
+        )}
         Donate Now <FaArrowRight />
       </button>
     </form>
