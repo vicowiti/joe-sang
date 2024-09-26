cc04202f348b6722e8db8985343cbb92a279696c
Victor <victor.owiti@zeraki.app>
2024-09-26 14:46:12 +0300

    Donate redirect

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,16 +6,23 @@ const Header = () => {
     <div>
       <section>
         <h1 className="text-center my-4 text-5xl font-bold">
-          <span className="font-extrabold">Bringing</span> Hope, <span className="font-extrabold">Changing</span> Lives
+          <span className="font-extrabold">Bringing</span> Hope,{" "}
+          <span className="font-extrabold">Changing</span> Lives
         </h1>
-        <p className="text-[#636363] text-lg text-center">From Barefoot Beginnings to Boundless Destinations,</p>
-        <p className="text-[#636363] text-lg text-center">A journey of Hope, Resilience and Transformation</p>
+        <p className="text-[#636363] text-lg text-center">
+          From Barefoot Beginnings to Boundless Destinations,
+        </p>
+        <p className="text-[#636363] text-lg text-center">
+          A journey of Hope, Resilience and Transformation
+        </p>
 
         <section className="my-3 flex justify-center items-center gap-3">
-          <button className="rounded-md py-3 px-4 text-white bg-[#0431FE] flex items-center gap-3">
-            Donate Now
-            <FaArrowRightLong />
-          </button>
+          <a href="#donate">
+            <button className="rounded-md py-3 px-4 text-white bg-[#0431FE] flex items-center gap-3">
+              Donate Now
+              <FaArrowRightLong />
+            </button>
+          </a>
           <button className="rounded-md py-3 px-4 text-[#232944] border border-[#9B9BFF] bg-[#fff] flex items-center gap-3">
             About Our Project
             <FaArrowRightLong />
