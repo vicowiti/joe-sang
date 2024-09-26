f2816c84d66d95d44d7887f4ee4b9fb53439edfa
Victor <victor.owiti@zeraki.app>
2024-09-26 14:24:59 +0300

    Donate redirect

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,15 +5,25 @@ const NavBar = () => {
   return (
     <div className="flex items-center px-10 justify-between">
       <div>
-        <Image src="/logo/logo.jpg" alt="Logo" width={150} height={80}/>
+        <Image src="/logo/logo.jpg" alt="Logo" width={150} height={80} />
       </div>
       <div>
         <ul className="flex items-center gap-10 text-lg text-[#000] mr-10 font-bold">
-          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">About Us</li>
-          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">Campaign</li>
-          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">Donation</li>
-          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">Blog</li>
-          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">Contact</li>
+          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">
+            About Us
+          </li>
+          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">
+            Campaign
+          </li>
+          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">
+            <a href="#donate">Donation</a>
+          </li>
+          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">
+            Blog
+          </li>
+          <li className="hover:text-[#9ca909] hover:scale-105 duration-500">
+            Contact
+          </li>
         </ul>
       </div>
     </div>
