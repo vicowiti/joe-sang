205e99a7c7a624236a57b806f96ddc58545c7a48
Victor <victor.owiti@zeraki.app>
2024-10-13 14:44:40 +0300

    newer first

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -20,59 +20,56 @@ export default function Blogs() {
           </p>
         </div>
         <div className="mx-auto mt-12 grid max-w-lg gap-5 lg:max-w-none lg:grid-cols-3">
-          {blogData.map((post) => (
+          {blogData.reverse().map((post) => (
             <div
-              
               key={post.title}
               className="flex flex-col overflow-hidden rounded-lg shadow-lg"
             >
-              <Link
-              href={`/blog/${post.id}`}>
-
-              <div className="flex-shrink-0">
-                <Image
-                  width={100}
-                  height={100}
-                  className="h-48 w-full object-cover"
-                  src={post.img}
-                  alt=""
-                />
-              </div>
-              <div className="flex flex-1 flex-col justify-between bg-white p-6">
-                <div className="flex-1">
-                  <p className="text-sm font-medium text-indigo-600">
-                    <span className="hover:underline">{`Article`}</span>
-                  </p>
-                  <span className="mt-2 block">
-                    <p className="text-xl font-semibold text-gray-900">
-                      {post.title}
-                    </p>
-                    <p className="mt-3 text-base text-gray-500">
-                      {post.content.slice(0, 100)}...
-                    </p>
-                  </span>
+              <Link href={`/blog/${post.id}`}>
+                <div className="flex-shrink-0">
+                  <Image
+                    width={100}
+                    height={100}
+                    className="h-48 w-full object-cover"
+                    src={post.img}
+                    alt=""
+                  />
                 </div>
-                <div className="mt-6 flex items-center">
-                  <div className="flex-shrink-0">
-                    <span>
-                      <span className="sr-only">{post.author}</span>
-                      <BsPersonFill
-                        color="#7d7fa1"
-                        className="h-10 w-10 rounded-full "
-                      />
+                <div className="flex flex-1 flex-col justify-between bg-white p-6">
+                  <div className="flex-1">
+                    <p className="text-sm font-medium text-indigo-600">
+                      <span className="hover:underline">{`Article`}</span>
+                    </p>
+                    <span className="mt-2 block">
+                      <p className="text-xl font-semibold text-gray-900">
+                        {post.title}
+                      </p>
+                      <p className="mt-3 text-base text-gray-500">
+                        {post.content.slice(0, 100)}...
+                      </p>
                     </span>
                   </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-medium text-gray-900">
-                      <span className="hover:underline">{post.author}</span>
-                    </p>
-                    <div className="flex space-x-1 text-sm text-gray-500">
-                      <span aria-hidden="true">&middot;</span>
-                      <span>4 min read</span>
+                  <div className="mt-6 flex items-center">
+                    <div className="flex-shrink-0">
+                      <span>
+                        <span className="sr-only">{post.author}</span>
+                        <BsPersonFill
+                          color="#7d7fa1"
+                          className="h-10 w-10 rounded-full "
+                        />
+                      </span>
+                    </div>
+                    <div className="ml-3">
+                      <p className="text-sm font-medium text-gray-900">
+                        <span className="hover:underline">{post.author}</span>
+                      </p>
+                      <div className="flex space-x-1 text-sm text-gray-500">
+                        <span aria-hidden="true">&middot;</span>
+                        <span>4 min read</span>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
               </Link>
             </div>
           ))}
