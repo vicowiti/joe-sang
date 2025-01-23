8ea17b3c2bc9bbf0c7cb1a2d15fc6be3fa01d729
Victor <victor.owiti@zeraki.app>
2025-01-23 07:44:36 +0300

    Back live

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-// import TopBanner from "./components/TopBanner";
-// import NavBar from "./components/NavBar";
-// import Footer from "./components/Footer";
+import TopBanner from "./components/TopBanner";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 const poppins_init = Montserrat({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -30,21 +30,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins_init.className} `}>
         <link rel="icon" href="/logo/favicon.ico" sizes="any" />
-        {/* <TopBanner />
+        <TopBanner />
         <NavBar />
         {children}
-        <Footer />  */}
-
-        <div className="h-screen w-screen flex justify-center items-center bg-black text-white font-bold text-2xl">
-          <p className="text-center">
-            {" "}
-            Service{" "}
-            <span className="text-red-600">Terminated until dev is paid.</span>
-          </p>
-        </div>
-        <p className="text-blue-500 underline my-3 font-bold">
-          <a href="mailto:viowiti12@gmail.com">Contact Dev</a>
-        </p>
+        <Footer />
       </body>
     </html>
   );
